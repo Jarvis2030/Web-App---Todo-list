df391774d6a0dcb49ff09a1e2bd373becf429675
Jarvis2020 <ylindc@connect.ust.hk>
2023-01-18 18:11:28 +0800

    1/11 backend setup

---
+++ todo_prac/backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors"); //cross origin resource 
+require("dotenv").config({ path: "./config.env" }); //access port
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
